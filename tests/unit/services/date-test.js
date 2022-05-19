597f70c5826629cb0e7ffa8eab8330f8074c9d91
Nathaniel Furniss <nlfurniss@gmail.com>
2022-05-19 16:01:36 -0700

    Add a test for Date.parse

--- tests/unit/services/date-test.js
+++ tests/unit/services/date-test.js
@@ -9,13 +9,25 @@ module('Unit | Service | date', function(hooks) {
   });
 
   test('it returns a date from the now method', function(assert) {
+    assert.expect(1);
+
     assert.ok(this.service.now());
   });
 
   test('it returns a date from the UTC method', function(assert) {
+    assert.expect(2);
+
     let date = this.service.UTC(71, 0, 23, 3, 4, 5);
 
     assert.ok(date);
     assert.equal(date.toUTCString(), 'Sat, 23 Jan 1971 03:04:05 GMT');
+  });
+
+  test('it returns a date from the parse method', function(assert) {
+    assert.expect(1);
+
+    let date = this.service.parse('04 Dec 1995 00:12:00 GMT');
+
+    assert.equal(date, 818035920000);
   });
 });
